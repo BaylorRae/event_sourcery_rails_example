d9632036cd33662cb34bbcfe583d97e691f3832d
Baylor Rae <baylor@thecodedeli.com>
2019-02-08 08:20:05 -0600

    configure event sourcery

---
+++ config/initializers/event_sourcery.rb
@@ -0,0 +1,53 @@
+module EventSourceryRails
+  class Config
+    attr_accessor :database_url
+  end
+
+  def self.config
+    @config ||= Config.new
+  end
+
+  def self.configure
+    yield config
+  end
+
+  def self.event_store
+    EventSourcery::Postgres.config.event_store
+  end
+
+  def self.event_source
+    EventSourcery::Postgres.config.event_store
+  end
+
+  def self.tracker
+    EventSourcery::Postgres.config.event_tracker
+  end
+
+  def self.event_sink
+    EventSourcery::Postgres.config.event_sink
+  end
+
+  def self.projections_database
+    EventSourcery::Postgres.config.projections_database
+  end
+
+  def self.repository
+    @repository ||= EventSourcery::Repository.new(
+      event_source: event_source,
+      event_sink: event_sink
+    )
+  end
+end
+
+EventSourceryRails.configure do |config|
+  config.database_url = ENV['DATABASE_URL'] || "postgres://127.0.0.1:5433/event_sourcery_rails_#{Rails.env}"
+end
+
+EventSourcery::Postgres.configure do |config|
+  database = Sequel.connect(EventSourceryRails.config.database_url)
+
+  # NOTE: Often we choose to split our events and projections into separate
+  # databases. For the purposes of this example we'll use one.
+  config.event_store_database = database
+  config.projections_database = database
+end
